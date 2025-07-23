9f4f4f37d3597d4b23961dc72b928e644f73ad67
hahamik <dlapdl0615@gmail.com>
2025-07-23 09:42:46 +0900

    1. 이메일 알림+기본알림

--- src/ex06/App.java
+++ src/ex06/App.java
@@ -3,18 +3,24 @@ package ex06;
 import ex06.notification.BasicNotifier;
 import ex06.notification.EmailNotifier;
 import ex06.notification.Notifier;
-import ex06.notification.SMSNotifier;
 
 public class App {
     public static void main(String[] args) {
-        Notifier b1 = new BasicNotifier();
-        b1.send();
-        System.out.println("__end");
-        Notifier e1 = new EmailNotifier();
-        e1.send();
-        System.out.println("__end");
-        Notifier s1 = new SMSNotifier();
-        s1.send();
+//        Notifier b1 = new BasicNotifier();
+//        b1.send();
+//        System.out.println("__end");
+//        Notifier e1 = new EmailNotifier();
+//        e1.send();
+//        System.out.println("__end");
+//        Notifier s1 = new SMSNotifier();
+//        s1.send();
+//        System.out.println("__end");
+
+        // 1. 이메일 알림 + 기본
+        Notifier b2 = new BasicNotifier();
+        Notifier e2 = new EmailNotifier();
+        b2.send();
+        e2.send();
         System.out.println("__end");
     }
 }
